36f241f4aa44ac8555fb5d730d4a89bd6edeb48c
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-09-03 02:03:10 +0700

    add file

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # pypromptpay
-ไปพร้อมเพย์
+QR Code PromptPay in Python 3.
+
+
+
+## install
+
+```
+pip install pypromptpay
+```
+
+## Using
+
+```python
+from pypromptpay import qr_code
+qr_code(account,one_time=True,path_qr_code="",country="TH",money="",currency="THB")
+```
+
+- account is phone number or  identification number.
+- one_time : if you use once than it's True.
+- path_qr_code : path save qr code.
+- country : TH
+- money : money (if have)
+- currency : THB
+
+## License
+
+Apache Software License 2.0
+
+
+
+## Develop
+
+Wannaphong Phatthiyaphaibun (wannaphong@kkumail.com)
---
+++ setup.py
@@ -0,0 +1,28 @@
+﻿# -*- coding: utf-8 -*-
+from setuptools import setup
+from setuptools import find_packages
+requirements = [
+    'qrcode',
+    'CRC-ITU'
+]
+
+setup(
+    name='pypromptpay',
+    version='0.4',
+    description="PromptPay QR code in Python",
+    author='Wannaphong Phatthiyaphaibun',
+    author_email='wannaphong@kkumail.com',
+    url='https://github.com/wannaphongcom/pypromptpay',
+    packages=['.'],
+    package_data={'': ['LICENSE','README.md']},
+    include_package_data=True,
+    install_requires=requirements,
+    license='Apache Software License 2.0',
+    zip_safe=False,
+    keywords='promptpay',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: Implementation'],
+)
