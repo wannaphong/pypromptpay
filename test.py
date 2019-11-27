a223e1195bda09436349a782a85ff1837099d7cd
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-11-27 15:44:16 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
+import unittest
 from pypromptpay import qr_code
-qr_code(account="0000000000000",one_time=True,path_qr_code="",country="TH",money="",currency="THB")
+class TestUM(unittest.TestCase):
+    def test_qrcode(self):
+        self.assertIsNotNone(qr_code(account="0000000000000",one_time=True,path_qr_code="",country="TH",money="",currency="THB"))
